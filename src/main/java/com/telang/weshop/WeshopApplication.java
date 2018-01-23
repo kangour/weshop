2a6512d102635329df81556cdc1034489d39e692
开朗 <laobingm@qq.com>
2018-01-23 23:25:33 +0800

    初始化spring boot2.0.0 M7框架

---
+++ src/main/java/com/telang/weshop/WeshopApplication.java
@@ -0,0 +1,12 @@
+package com.telang.weshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeshopApplication.class, args);
+	}
+}
