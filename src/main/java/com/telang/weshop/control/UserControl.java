a7ea5a4fb969127bdd0ec06b9b1aa26ba8739cb8
tanj <laobingm@qq.com>
2018-02-12 18:03:37 +0800

    th模板收官

--- src/main/java/com/telang/weshop/control/UserControl.java
+++ src/main/java/com/telang/weshop/control/UserControl.java
@@ -1,28 +1,20 @@
 package com.telang.weshop.control;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.telang.weshop.entity.UserInfo;
 
-
-@RestController
+@Controller
 public class UserControl {
-    
-    @RequestMapping("/")
-    public ModelAndView test(ModelAndView mv) {
-        mv.setViewName("index.html");
-        mv.addObject("status",0);
-        return mv;
-    }    
-    @RequestMapping("/login333")
-    public ModelAndView test2(ModelAndView mv) {
-        mv.setViewName("login.html");
-        mv.addObject("name","Kangour");
-        mv.addObject("message","你的到来小站感到很荣幸！");
-        
-        return mv;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String page(Model model) {
+        UserInfo user = new UserInfo();
+        user.setUsername("Kangour");
+        user.setPassword("123456");
+        model.addAttribute("user",user);
+        return "index.html";
     }
 }
