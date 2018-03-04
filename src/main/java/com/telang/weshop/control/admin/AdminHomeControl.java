f1d468ff9886478101f4c31666e9347253806540
zhangxy <1281471119@qq.com>
2018-03-04 13:19:40 +0800

    提交后台管理员登陆

--- src/main/java/com/telang/weshop/control/admin/AdminHomeControl.java
+++ src/main/java/com/telang/weshop/control/admin/AdminHomeControl.java
@@ -8,4 +8,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AdminHomeControl {
    
+	  /**
+	   * 管理员后台登陆
+	   * @author zhangxy
+	   * @param model
+	   * @return
+	   */
+	@RequestMapping(value="admin/home/adminLogin", method = RequestMethod.GET)
+    public String productList(Model model) {
+        return "admin/home/adminLogin.html";
+    }
 }
