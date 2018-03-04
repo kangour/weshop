7c5ea0fc6b86231a80ed39ff505dd768d0a7ccee
开朗 <laobingm@qq.com>
2018-03-04 10:38:02 +0800

    提交产品评论。

--- src/main/java/com/telang/weshop/control/admin/AdminProductControl.java
+++ src/main/java/com/telang/weshop/control/admin/AdminProductControl.java
@@ -7,6 +7,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminProductControl {
-
-   
+    /***
+     * 后台订单列表
+     * @param model
+     * @return
+     */
+    @RequestMapping(value="admin/product/orderList", method = RequestMethod.GET)
+    public String orderList(Model model) {
+        return "admin/product/orderList.html";
+    }
+    /***
+     * 后台评论列表
+     * @param model
+     * @return
+     */
+    @RequestMapping(value="admin/product/comment", method = RequestMethod.GET)
+    public String comment(Model model) {
+        return "admin/product/comment.html";
+    }
 }
