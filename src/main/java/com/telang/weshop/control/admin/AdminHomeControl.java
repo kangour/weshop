cddc233fc69dfa21611e7439d0bd543cf74020fd
开朗 <laobingm@qq.com>
2018-03-18 20:53:25 +0800

    完成接口，根据前端ajax传来的id查询商品。查询全部商品。删除还没完成。添加测试页/apiTest.html

--- src/main/java/com/telang/weshop/control/admin/AdminHomeControl.java
+++ src/main/java/com/telang/weshop/control/admin/AdminHomeControl.java
@@ -2,12 +2,24 @@ package com.telang.weshop.control.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminHomeControl {
 
+    /**
+     * 测试页面：前端ajax请求api数据 
+     * 
+     * @author 开朗
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "apiTest.html", method = RequestMethod.GET)
+    public String apiTest(Model model) {
+        return "admin/home/apiTest.html";
+    }
     /**
      * 管理员后台登陆
      * 
@@ -41,5 +53,15 @@ public class AdminHomeControl {
     @RequestMapping(value = "admin/home/indexContent.html", method = RequestMethod.GET)
     public String indexContent(Model model) {
         return "admin/home/indexContent.html";
+    } 
+    /***
+     * 错误页
+     * @param model
+     * @return
+     */   
+    @GetMapping("/error")
+    public String errPage(Model model) { 
+        return "admin/home/error.html";
     }
+
 }
