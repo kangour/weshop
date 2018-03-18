827370143653257e01cf43b018cd6ccb56ab0307
开朗 <laobingm@qq.com>
2018-03-18 14:37:47 +0800

    后台首页结构调整。

--- src/main/java/com/telang/weshop/control/admin/AdminHomeControl.java
+++ src/main/java/com/telang/weshop/control/admin/AdminHomeControl.java
@@ -7,16 +7,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminHomeControl {
-   
-	  /**
-	   * 管理员后台登陆
-	   * @author zhangxy
-	   * @param model
-	   * @return
-	   */
-	@RequestMapping(value="admin/home/adminLogin", method = RequestMethod.GET)
+
+    /**
+     * 管理员后台登陆
+     * 
+     * @author zhangxy
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "admin/adminLogin", method = RequestMethod.GET)
     public String productList(Model model) {
         return "admin/home/adminLogin.html";
     }
-	
+
+    /**
+     * 后台首页
+     * 
+     * @author wangchengyang
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "admin/index", method = RequestMethod.GET)
+    public String index(Model model) {
+        return "admin/home/index.html";
+    }
 }
