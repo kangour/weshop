cb398bd388b489a30a627238162deafaa69af834
soulsjie <1186188771@qq.com>
2018-03-20 09:40:54 +0800

    完成用户信息增删改查的API

--- src/main/java/com/telang/weshop/api/UserInfoApi.java
+++ src/main/java/com/telang/weshop/api/UserInfoApi.java
@@ -3,6 +3,7 @@ package com.telang.weshop.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telang.weshop.dao.UserInfoMapper;
@@ -23,7 +24,7 @@ public class UserInfoApi {
      * 获取用户列表
      * @return  
      */
-    @RequestMapping(value = "/api/v1/get/user_info/all", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/v1/get/userinfo/all", method = RequestMethod.GET)
     public UserInfo[] GetAllUserInfo() {
         
         return use.selectAll();
@@ -33,8 +34,76 @@ public class UserInfoApi {
      * 按用户名获取用户信息
      * @return  
      */
-    @RequestMapping(value = "/api/v1/get/user_info/username", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/v1/get/userinfo/username", method = RequestMethod.GET)
     UserInfo selectByUsername(String username) {
-        return use.selectByUsername("soulsjie");
+        return use.selectByUsername(username);
+    }
+    
+    /***
+     * 根据id删除用户信息
+     * 
+     * @return
+     */
+    @RequestMapping(value = "/api/v1/delete/userinfo/id", method = RequestMethod.DELETE)
+    public int DeleteUserInfoById(@RequestParam("id") Integer id) {
+
+        return use.deleteByPrimaryKey(id);
+    }
+    /***
+     * 插入一条用户信息记录
+     * 
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/api/v1/post/userinfo/a", method = RequestMethod.POST)
+    public int PostUserInfo(@RequestParam(value = "id", required = false) Integer id,
+            @RequestParam(value = "username", required = false) String username,
+            @RequestParam(value = "password", required = false) String password,
+            @RequestParam(value = "status", required = false) String status,
+            @RequestParam(value = "email", required = false) String email,
+            @RequestParam(value = "address_id", required = false) Integer address_id,
+            @RequestParam(value = "idcard", required = false) String idcard,
+            @RequestParam(value = "phone", required = false) String phone,
+            @RequestParam(value = "user_img", required = false) String user_img
+            ) {
+        UserInfo us = new UserInfo();
+        us.setId(id);
+        us.setUsername(username);
+        us.setPassword(password);
+        us.setStatus(status);
+        us.setEmail(email);
+        us.setAddressId(address_id);
+        us.setIdcard(idcard);
+        us.setPhone(phone);
+        us.setUserImg(user_img);
+        return use.insertSelective(us);
+    }
+    /***
+     * 根据id修改用户信息
+     * 
+     * @return
+     */
+    @RequestMapping(value = "/api/v1/patch/userinfo/id", method = RequestMethod.PATCH)
+    public int PatchUserInfoById(@RequestParam(value = "id", required = false) Integer id,
+            @RequestParam(value = "username", required = false) String username,
+            @RequestParam(value = "password", required = false) String password,
+            @RequestParam(value = "status", required = false) String status,
+            @RequestParam(value = "email", required = false) String email,
+            @RequestParam(value = "address_id", required = false) Integer address_id,
+            @RequestParam(value = "idcard", required = false) String idcard,
+            @RequestParam(value = "phone", required = false) String phone,
+            @RequestParam(value = "user_img", required = false) String user_img
+            ) {
+        UserInfo us = new UserInfo();
+        us.setId(id);
+        us.setUsername(username);
+        us.setPassword(password);
+        us.setStatus(status);
+        us.setEmail(email);
+        us.setAddressId(address_id);
+        us.setIdcard(idcard);
+        us.setPhone(phone);
+        us.setUserImg(user_img);
+        return use.updateByPrimaryKeySelective(us);
     }
 }
