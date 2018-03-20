339f366654ef728b80b7e1c569b08c4cbf784db0
wangchengyang <179114223@qq.com>
2018-03-20 15:11:14 +0800

    修复订单借口的请求方法错误。

--- src/main/java/com/telang/weshop/api/OrderApi.java
+++ src/main/java/com/telang/weshop/api/OrderApi.java
@@ -15,82 +15,83 @@ import com.telang.weshop.entity.OrderInfo;
  */
 @RestController
 public class OrderApi {
-    // 定义一个产品接口类对象
-    @Autowired
-    private OrderInfoMapper order;
+	// 定义一个产品接口类对象
+	@Autowired
+	private OrderInfoMapper order;
 
-    /***
-     * 获取所有订单信息
-     * 
-     * @return
-     */
-    @RequestMapping(value = "/api/v1/get/order_info/all", method = RequestMethod.GET)
-    public OrderInfo[] GetAllOrderInfo() {
+	/***
+	 * 获取所有订单信息
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/api/v1/get/order_info/all", method = RequestMethod.GET)
+	public OrderInfo[] GetAllOrderInfo() {
 
-        return order.selectAll();
-    }
-    
-    /***
-     * 根据订单id获取信息
-     * 
-     * @return
-     */
-    @RequestMapping(value = "/api/v1/get/order_info/productid", method = RequestMethod.GET)
-   OrderInfo SelectByProductid(Integer productid) {
+		return order.selectAll();
+	}
 
-        return order.selectByPrimaryKey(productid);
-    }
-    /***
-     * 插入一条数据
-     * 
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "/api/v1/patch/order_info/a", method = RequestMethod.POST)
-    public int PostProduct(@RequestParam(value = "id", required = false) Integer id,
-            @RequestParam(value = "product_id", required = false) Integer product_id,
-            @RequestParam(value = "price", required = false) Double price,
-            @RequestParam(value = "status_id", required = false) Integer status_id)
-            {
+	/***
+	 * 根据订单id获取信息
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/api/v1/get/order_info/id", method = RequestMethod.GET)
+	OrderInfo SelectByid(Integer id) {
 
-    	OrderInfo or = new OrderInfo();
+		return order.selectByPrimaryKey(id);
+	}
 
-    	or.setId(id);
-    	or.setProductId(product_id);
-    	or.setPrice(price);
-    	or.setStatusId(status_id);
-        return order.insertSelective(or);
-    }
-    /***
-     * 根据订单id删除信息
-     * 
-     * @return
-     */
-    @RequestMapping(value = "/api/v1/delete/order_info/productid", method = RequestMethod.DELETE)
-     public int DeleteByProductid(@RequestParam("id") Integer productid) {
+	/***
+	 * 插入一条数据
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/api/v1/post/order_info/a", method = RequestMethod.POST)
+	public int PostProduct(@RequestParam(value = "id", required = false) Integer id,
+			@RequestParam(value = "product_id", required = false) Integer product_id,
+			@RequestParam(value = "price", required = false) Double price,
+			@RequestParam(value = "status_id", required = false) Integer status_id) {
 
-        return order.deleteByPrimaryKey(productid);
-    }
-    /***
-     * 根据订单id修改信息
-     * 
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "/api/v1/post/order_info/productid", method = RequestMethod.PATCH)
-      public int PatchProduct(@RequestParam(value = "id", required = false) Integer id,
-            @RequestParam(value = "product_id", required = false) Integer product_id,
-            @RequestParam(value = "price", required = false) Double price,
-            @RequestParam(value = "status_id", required = false) Integer status_id)
-            {
+		OrderInfo or = new OrderInfo();
 
-    	OrderInfo or = new OrderInfo();
+		or.setId(id);
+		or.setProductId(product_id);
+		or.setPrice(price);
+		or.setStatusId(status_id);
+		return order.insertSelective(or);
+	}
 
-    	or.setId(id);
-    	or.setProductId(product_id);
-    	or.setPrice(price);
-    	or.setStatusId(status_id);
-        return order.updateByPrimaryKeySelective(or);
-    }
+	/***
+	 * 根据订单id删除信息
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/api/v1/delete/order_info/id", method = RequestMethod.DELETE)
+	public int DeleteByid(@RequestParam("id") Integer id) {
+
+		return order.deleteByPrimaryKey(id);
+	}
+
+	/***
+	 * 根据订单id修改信息
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/api/v1/patch/order_info/id", method = RequestMethod.PATCH)
+	public int PatchProduct(@RequestParam(value = "id", required = false) Integer id,
+			@RequestParam(value = "product_id", required = false) Integer product_id,
+			@RequestParam(value = "price", required = false) Double price,
+			@RequestParam(value = "status_id", required = false) Integer status_id) {
+
+		OrderInfo or = new OrderInfo();
+
+		or.setId(id);
+		or.setProductId(product_id);
+		or.setPrice(price);
+		or.setStatusId(status_id);
+		return order.updateByPrimaryKeySelective(or);
+	}
 
 }
