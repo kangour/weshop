36ec4f4774b54c087b504b3fc718216005661a16
laobingm <laobingm@qq.com>
2018-03-21 15:13:05 +0800

    1

--- src/main/java/com/telang/weshop/dao/ProductMapper.java
+++ src/main/java/com/telang/weshop/dao/ProductMapper.java
@@ -1,28 +1,25 @@
 package com.telang.weshop.dao;
 
-
 import com.telang.weshop.entity.Product;
 import com.telang.weshop.entity.ProductDetail;
 
 public interface ProductMapper {
-    int deleteByPrimaryKey(Integer id);
+	int deleteByPrimaryKey(Integer id);
 
-    int insert(Product record);
+	int insert(Product record);
 
-    int insertSelective(Product record);
+	int insertSelective(Product record);
 
-    Product selectByPrimaryKey(Integer id);
+	Product selectByPrimaryKey(Integer id);
 
-    // 查询所有商品数据
-    Product[] selectAll();
-    
-    int updateByPrimaryKeySelective(Product record);
+	// 查询所有商品数据
+	Product[] selectAll();
 
-    int updateByPrimaryKey(Product record);
+	int updateByPrimaryKeySelective(Product record);
+
+	int updateByPrimaryKey(Product record);
 
 	ProductDetail selectDetailById(Integer id);
-
-	ProductDetail[] selectAllDetail();
 
 	ProductDetail[] selectAllDetail(String key);
 
