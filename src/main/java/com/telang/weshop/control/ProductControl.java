377c751e3151826ff002c5a76ae65b4d9eab8181
zhangxy <1281471119@qq.com>
2018-03-22 17:29:18 +0800

    点击商品进入详情页

--- src/main/java/com/telang/weshop/control/ProductControl.java
+++ src/main/java/com/telang/weshop/control/ProductControl.java
@@ -1,13 +1,20 @@
 package com.telang.weshop.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.telang.weshop.dao.ProductMapper;
+import com.telang.weshop.entity.Product;
 
 @Controller
 public class ProductControl {
 
+	@Autowired
+	private ProductMapper prod;
     /*** 
      * 搜索页
      * @param model
@@ -24,8 +31,11 @@ public class ProductControl {
      * @return
      */
     @RequestMapping(value = "product/introduction", method = RequestMethod.GET)
-    public String introduction(Model model) {
-        return "product/introduction.html";
+    public String GetProductById(@RequestParam(value = "id", required = false) Integer id,Model model) {
+		     Product  product=prod.selectByPrimaryKey(id);
+		      model.addAttribute("product", product);    	
+            return "product/introduction.html";
+    	
     }
     
     /**
